40ddcedd03f5942159a25eef6885eb9492839ba1
sinsenti <sinsenti73@gmail.com>
2025-07-11 12:22:57 +0300

    Auto-push: 2025-07-11 12:22:57

--- game.cpp
+++ game.cpp
@@ -4,7 +4,6 @@
 #include <QImage>
 #include <QPainter>
 
-// MODIFIED: Constructor now accepts a Difficulty parameter
 Game::Game(Difficulty difficulty, QWidget *parent)
     : QGraphicsView(parent), currentDifficulty(difficulty) {
   scene = new QGraphicsScene(this);
